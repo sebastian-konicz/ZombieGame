0e49f15afcb82da9d31a14ab2e4f4bbe8c407f52
Sebastian Konicz <sebastian.konicz@gmail.com>
2018-06-22 15:09:46 +0200

    Podstawy Gry

---
+++ js/game.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("lalala");
+
+    var board = document.querySelector(".plansza");
+
+    timer = setInterval(function(){
+        var zombie = document.createElement("div");
+        zombie.classList.add("zombie");
+
+        var boom = document.createElement("div");
+        boom.classList.add("boom");
+
+        board.appendChild(zombie);
+
+
+        //Losujemy liczbę od 10 do horyzont
+        // trzeba elementowi zombie dodać style
+
+        //Mieksce startu
+        minPosition = 10;
+        maxPosition = 300;
+        randomPosition = Math.floor(Math.random() * (maxPosition - minPosition + 1)) + minPosition;
+
+        zombie.style.bottom = randomPosition + "px";
+
+        //Szybkość zombie
+        minSpeed = 3;
+        maxSpeed = 10;
+        randomSpeed = Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed;
+        zombie.style.animationDuration = "0.5s," + randomSpeed + "s"
+
+        //Wielkość zombie
+        scale = 1 - (randomPosition/350);
+        zombie.style.transform = "scale(" + scale +")";
+
+        //Z-index Zombie
+        index = 350 - randomPosition;
+        zombie.style.zIndex = index;
+
+
+        zombie.addEventListener("animationend", function(event){
+            this.remove();
+        });
+
+        zombie.addEventListener("click", function(event){
+            this.remove();
+
+            var positionX= event.screenX;
+            var positionY = event.screenY;
+            console.log(positionX);
+            console.log(positionY);
+            var boom = document.createElement("div");
+            boom.classList.add("boom");
+            boom.style.left = positionX +"px";
+            boom.style.top = positionY +"px";
+
+            board.appendChild(boom);
+            boom.addEventListener("animationend", function(event){
+                this.remove();
+
+            });
+        });
+
+    }, 1000)
+})
